8133930e23fdd3785cbac19d6f46710eda6c5a24
prawar <prawar.poudel@hotmail.com>
2019-04-19 18:54:07 -0500

    wrote program to handle and transer file, but error: address already in use

--- client/client.py
+++ client/client.py
@@ -1,20 +1,57 @@
 import sys
 import socket
 
+debug = True
+
 def function(host,port):
 	s = socket.socket()
 	s.connect((host,port))
 	print(s.recv(1024))
 	s.close()
 
+def functionRecvFile(host,port,chunk_size=1024,filename = "test.dat"):
+	# create a socket and conenct to the host
+	if debug:
+		print(f"..opening socket for connection")
+	s = socket.socket()
+	if debug:
+		print(f"..connecting to the host {host}:{port}")
+	try:
+		s.connect((host,port))
+	except Exception as e:
+		print(f"....error in connecting to the host: {e}")
+		s.close()
+		return
+
+	with open(filename,'wb') as file_handler:
+		l = s.recv(chunk_size)
+		while(l):
+			file_handler.write(l)
+			l = s.recv(chunk_size)
+		if debug:
+			print(f"..finished reading the file from server")
+	s.close()
+	return
+
 if __name__=='__main__':
 	print("attempting connection")
 	host = str(socket.gethostbyname(socket.gethostname()))
 	port = 12345
+	filename = 'test.dat'
+	chunk_size = 1024
 	if len(sys.argv)==2:
 		host = sys.argv[1]
-	elif len(sys.argv)>2:
+	elif len(sys.argv)==3:
 		host = sys.argv[1]
 		port = sys.argv[2]
+	elif len(sys.argv)==4:
+		host = sys.argv[1]
+		port = sys.argv[2]
+		chunk_size = sys.argv[3]
+	elif len(sys.argv)>4:
+		host = sys.argv[1]
+		port = sys.argv[2]
+		chunk_size = sys.argv[3]
+		filename = sys.argv[4]
 	
-	function(host,port)
+	functionRecvFile(host,int(port),chunk_size,filename)
--- server/server.py
+++ server/server.py
@@ -1,12 +1,22 @@
 import sys
 import socket
 
+debug = True
+# the following function in server listens to a port
+# ..once there is a request in the port, server sends a string message
 def function(port,message="Thank you for connecting"):
 	s=  socket.socket()
+
 	print(f"socket created successfully")
 	
-	s.bind(('',port))
-	print(f"socker bound successfully to port {port}")
+	try:
+		s.bind(('',port))
+	except Exception as e:
+		print(f"....error in binding the socket: {e}")
+		s.close()
+		return
+
+	print(f"socket bound successfully to port {port}")
 	s.listen(5)
 	print(f"..listening")
 	
@@ -19,16 +29,58 @@ def function(port,message="Thank you for connecting"):
 			continue
 		else:
 			return
+	s.close()
+
+def functionSendFile(port,chunk_size = 1024,filename='test.dat'):
+	s = socket.socket()
+
+	if debug:
+		print(f"socket created successfully")
+	
+	try:
+		s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+		s.bind(('',port))
+	except Exception as e:
+		print(f"....error in binding the socket: {e}")
+		s.close()
+		return
+	
+	print(f"socket bound successfully to port {port}")
+	s.listen(5)
+	print(f"..listening")
+	
+	while True:
+		c,addr = s.accept()
+		print(f"..got connection from {addr}")
+		c.send(message.encode())
+		with open(filename,'rb') as file_handler:
+			l = file_handler.read(chunk_size)
+			while (l):
+				c.send(l)
+				l = file_handler.read(chunk_size)
+		c.close()
+		if debug:
+			print(f".. sent file completed")
+			print(f"..closing connection")
+	return
 
 
 if __name__=="__main__":
 	port = 12345
+	chunk_size = 1024
+	filename = 'test.dat'
+
 	if len(sys.argv)==1:
-		function(port)
+		functionSendFile(port)
 	elif len(sys.argv)==2:
 		port = int(sys.argv[1])
-		function(port)
-	else:
+		functionSendFile(port)
+	elif len(sys.argv)==3:
 		port = int(sys.argv[1])
-		message = sys.argv[2]
-		function(port,message)
+		chunk_size = int(sys.argv[2])
+		functionSendFile(port,chunk_size)
+	elif len(sys.argv)>=4:
+		port = int(sys.argv[1])
+		chunk_size = int(sys.argv[2])
+		filename = sys.argv[3]
+		functionSendFile(port,chunk_size,filename)
